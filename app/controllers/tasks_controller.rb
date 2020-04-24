bc9827da686de505163754ca898b88039f8708e2
yuki takagi <yuki99takagi@gmail.com>
2020-04-24 16:38:03 +0900

    [Add] タスク一覧,タスク登録,タスク編集機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,34 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.title}」を登録しました。"
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.title}」を更新しました。"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
