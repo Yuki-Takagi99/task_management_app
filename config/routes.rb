4c7dc322eb43ee88c9c51dd851f04ba51801ffa2
yuki takagi <yuki99takagi@gmail.com>
2020-04-24 15:01:30 +0900

    [Update] routes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  resources :tasks
 end
