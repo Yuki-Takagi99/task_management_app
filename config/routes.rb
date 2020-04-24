8ee58e123c0a9150eadbdcbbd2a27a77827c61df
yuki takagi <yuki99takagi@gmail.com>
2020-04-24 14:51:29 +0900

    [Add] tasksコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
