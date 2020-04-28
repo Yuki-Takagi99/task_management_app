38299aaba6eac8f86b294ee3d78d90ac59e29932
yuki takagi <yuki99takagi@gmail.com>
2020-04-28 17:33:11 +0900

    [Add] tasksテーブルにend_deadlineカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_093150) do
+ActiveRecord::Schema.define(version: 2020_04_28_081551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_093150) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "end_deadline", default: -> { "now()" }, null: false
   end
 
 end
