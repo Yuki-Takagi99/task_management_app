f3e0b7fdeba467346e45db755d4ffc559178a2e5
yuki takagi <yuki99takagi@gmail.com>
2020-04-29 11:52:10 +0900

    [Update] 終了期限の昇順でソートするように修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def index
     if params[:sort_expired]
-      @tasks = Task.all.order(end_deadline: :desc) #終了期限の降順に表示
+      @tasks = Task.all.order(end_deadline: :asc) #終了期限の昇順に表示
     else
       @tasks = Task.all.order(created_at: :desc) #作成日時の降順に表示
     end
