9f0d9802bafe1bd1645c9657b0cacc0d8e0499bc
yuki takagi <yuki99takagi@gmail.com>
2020-04-29 15:24:44 +0900

    [Add] 終了期限でソートするテストを追記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'test_title_01' }
     description { 'test_description_01' }
+    end_deadline { '2020-06-01' }
   end
   factory :second_task, class: Task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'test_title_02' }
     description { 'test_description_02' }
+    end_deadline { '2020-06-02' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -19,25 +19,23 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(task_list[1]).to have_content 'test_title_01'
       end
     end
+    context '終了期限でソートするボタンを押した場合' do
+      it 'タスクが終了期限順の昇順に並んでいる' do
+        visit tasks_path
+        click_on '終了期限でソートする'
+        task_list = all('.deadline_row')
+        expect(task_list[0]).to have_content '2020-06-01'
+        expect(task_list[1]).to have_content '2020-06-02'
+      end
+    end
   end
   describe 'タスク登録画面' do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存される' do
-        # new_task_pathにvisitする（タスク登録ページに遷移する）
-        # 1.ここにnew_task_pathにvisitする処理を書く
         visit new_task_path
-        # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
-        # タスクのタイトルと内容をそれぞれfill_in（入力）する
-        # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'タイトル', with: 'test_title_03'
-        # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in '詳細', with: 'test_description_03'
-        # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
-        # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
         click_on '登録する'
-        # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-        # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-        # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
         expect(page).to have_content 'test_title_03'
       end
     end
@@ -45,13 +43,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
-         # テストで使用するためのタスクを作成
          task = FactoryBot.create(:task, title: 'test_title_04', description: 'test_description_04')
-         # タスク一覧ページに遷移
          visit tasks_path
-         # 作成したタスクのtitleをクリックし、タスク詳細ページに遷移
          click_on 'test_title_04'
-         # タスク詳細ページに、作成した内容が含まれているかを確認
          expect(page).to have_content 'test_description_04'
        end
      end
