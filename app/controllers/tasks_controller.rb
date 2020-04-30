90a91b252dc95c3705994444f85a9140e6c1cde9
yuki takagi <yuki99takagi@gmail.com>
2020-04-30 17:42:26 +0900

    [Update] 検索ロジックをscopeで定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,11 @@ class TasksController < ApplicationController
   def index
     if params[:search].present?
       if params[:title].present? && params[:status].present? #タイトルとステータス両方で検索する場合 #タイトルはあいまい検索
-        @tasks = Task.where("title LIKE ?", "%#{params[:title]}%").where(status: params[:status])
+        @tasks = Task.search_title(params[:title]).search_status(params[:status])
       elsif params[:title].present? #タイトルのみで検索する場合
-        @tasks = Task.where("title LIKE ?", "%#{params[:title]}%")
+        @tasks = Task.search_title(params[:title])
       else params[:status].present? #ステータスのみで検索する場合
-        @tasks = Task.where(status: params[:status])
+        @tasks = Task.search_status(params[:status])
       end
     elsif params[:sort_expired]
       @tasks = Task.all.order(end_deadline: :asc) #終了期限の昇順に表示
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30}
   validates :description, presence: true
+
+  scope :search_title, -> (search_title) { where("title LIKE ?", "%#{search_title}%") }
+  scope :search_status, -> (search_status) { where(status: search_status) }
 end
