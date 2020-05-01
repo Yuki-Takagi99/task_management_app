d29446bd09ccc72bcbd6dd4df0a345149b119ac1
yuki takagi <yuki99takagi@gmail.com>
2020-05-01 10:53:53 +0900

    [Add] modelスペックテストを追記

--- spec/models/task.rb
+++ spec/models/task.rb
@@ -12,4 +12,21 @@ RSpec.describe 'タスク管理機能', type: :model do
     task = Task.new(title: '成功テスト', description: '成功テスト')
     expect(task).to be_valid
   end
+  describe '検索機能' do
+    context 'scopeメソッドで検索をした場合' do
+      before do
+        Task.create(title: "task", description: "sample_task", status: "着手中")
+        Task.create(title: "sample", description: "sample_sample")
+      end
+      it "scopeメソッドでタイトル検索ができる" do
+        expect(Task.search_title('task').count).to eq 1
+      end
+      it "scopeメソッドでステータス検索ができる" do
+        expect(Task.search_status('着手中').count).to eq 1
+      end
+      it "scopeメソッドでタイトルとステータスの両方が検索できる" do
+        expect(Task.search_title('task').search_status('着手中').count).to eq 1
+      end
+    end
+  end
 end
