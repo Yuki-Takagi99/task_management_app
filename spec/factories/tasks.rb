b5d4917fda9451163a01dea5e8145b6505f13f4b
yuki takagi <yuki99takagi@gmail.com>
2020-05-03 17:23:30 +0900

    [Update] システムスペックのテストコードを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,20 +1,41 @@
 FactoryBot.define do
   factory :task do
-    title { 'test_title_01' }
-    description { 'test_description_01' }
+    sequence(:title) { |n| "test_title_#{n}"}
+    sequence(:description) { |n| "test_description_#{n}"}
     end_deadline { '2020-06-01' }
     status { '着手中' }
     priority { '低' }
   end
-  factory :second_task, class: Task do
-    title { 'test_title_02' }
-    description { 'test_description_02' }
+  factory :deadline_first, class: Task do
+    title { "deadline_first" }
+    description { "deadline_first" }
     end_deadline { '2020-06-02' }
     priority { '中' }
   end
-  factory :third_task, class: Task do
-    title { 'test_title_03' }
-    description { 'test_description_03' }
+  factory :deadline_second, class: Task do
+    title { "deadline_second" }
+    description { "deadline_second" }
+    end_deadline { '2020-06-03' }
+    status { '未着手' }
+    priority { '高' }
+  end
+  factory :low_priority, class: Task do
+    title { "low_priority" }
+    description { "low_priority" }
+    end_deadline { '2020-06-03' }
+    status { '未着手' }
+    priority { '低' }
+  end
+  factory :middle_priority, class: Task do
+    title { "middle_priority"}
+    description { "middle_priority" }
+    end_deadline { '2020-06-03' }
+    status { '未着手' }
+    priority { '中' }
+  end
+  factory :high_priority, class: Task do
+    title { "high_priority"}
+    description { "high_priority" }
     end_deadline { '2020-06-03' }
     status { '未着手' }
     priority { '高' }
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  before do
-    create(:task)
-    create(:second_task)
-    create(:third_task)
-  end
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
+        create(:task)
         visit tasks_path
-        expect(page).to have_content 'test_title_01'
+        expect(page).to have_content 'test_title_1'
       end
     end
     context '複数のタスクを作成した場合' do
       it 'タスクが作成日時の降順に並んでいる' do
+        create_list(:task, 3)
         visit tasks_path
         task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
-        expect(task_list[0]).to have_content 'test_title_03'
-        expect(task_list[1]).to have_content 'test_title_02'
-        expect(task_list[2]).to have_content 'test_title_01'
+        expect(task_list[0]).to have_content 'test_title_4'
+        expect(task_list[1]).to have_content 'test_title_3'
+        expect(task_list[2]).to have_content 'test_title_2'
       end
     end
     context '終了期限でソートするボタンを押した場合' do
       it 'タスクが終了期限の昇順に並んでいる' do
+        create(:task)
+        create(:deadline_first)
         visit tasks_path
         click_on '終了期限でソートする'
         sleep 0.5 # テスト失敗を回避
@@ -33,6 +32,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context '優先順位でソートするボタンを押した場合' do
       it '優先順位が高・中・低の順で並んでいる' do
+        create(:low_priority)
+        create(:middle_priority)
+        create(:high_priority)
         visit tasks_path
         click_on '優先順位でソートする'
         sleep 0.5 # テスト失敗を回避
@@ -43,11 +45,14 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
     context '検索をした場合' do
+      before do
+        create(:task)
+      end
       it "タイトルで検索できる" do
         visit tasks_path
-        fill_in 'タイトル', with: 'test_title_01'
+        fill_in 'タイトル', with: 'test_title_6'
         click_on '検索'
-        expect(page).to have_content 'test_title_01'
+        expect(page).to have_content 'test_title_6'
       end
       it "ステータスで検索できる" do
         visit tasks_path
@@ -57,10 +62,10 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
       it 'タイトルとステータスの両方で検索できる' do
         visit tasks_path
-        fill_in 'タイトル', with: 'test_title_01'
+        fill_in 'タイトル', with: 'test_title_8'
         select '着手中', from: '状態'
         click_on '検索'
-        expect(page).to have_content 'test_title_01'
+        expect(page).to have_content 'test_title_8'
         expect(page).to have_content '着手中'
       end
     end
@@ -69,20 +74,20 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存される' do
         visit new_task_path
-        fill_in 'タイトル', with: 'test_title_03'
-        fill_in '詳細', with: 'test_description_03'
+        fill_in 'タイトル', with: 'create_test_title'
+        fill_in '詳細', with: 'create_test_description'
         click_on '登録する'
-        expect(page).to have_content 'test_title_03'
+        expect(page).to have_content 'create_test_title'
       end
     end
   end
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
-         task = create(:task, title: 'test_title_04', description: 'test_description_04')
+         task = create(:task, title: 'show_test_title', description: 'show_test_description')
          visit tasks_path
-         click_on 'test_title_04'
-         expect(page).to have_content 'test_description_04'
+         click_on 'show_test_title'
+         expect(page).to have_content 'show_test_description'
        end
      end
   end
