0c84cbfb63b89eddea47d2316dc91aca8113ede2
yuki takagi <yuki99takagi@gmail.com>
2020-05-05 21:17:58 +0900

    [Add] Userモデルにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
