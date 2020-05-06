2f56da53bfd6d18dabdbeeff427c95524a20af4d
yuki takagi <yuki99takagi@gmail.com>
2020-05-06 19:02:26 +0900

    [Update] シードデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
-100.times do |n|
-  Task.create(title:"テストタイトル#{n}", description:"テスト詳細#{n}")
+
+User.create!(
+  name: 'admin',
+  email: 'admin@test.com',
+  password: 'adminadmin',
+  password_confirmation: 'adminadmin',
+  admin: 'true',
+)
+
+5.times do |n|
+  User.create!(
+    name: "ユーザー#{n + 1}",
+    email: "user#{n + 1}@test.com",
+    password: "useruser#{n + 1}",
+    password_confirmation: "useruser#{n + 1}",
+    admin: "false",
+  )
 end
