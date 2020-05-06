f4acbaa1c5dde49884ddc0f378a3008e77833cf6
yuki takagi <yuki99takagi@gmail.com>
2020-05-06 10:01:08 +0900

    [Add] Userコントローラ、new画面、show画面を作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root to: 'tasks#index'
   resources :tasks
+  resources :users, only: [:new, :create, :show]
 end
