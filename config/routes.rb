7cb10eb2fa7fff3a5584c85e1b43a674027cf82b
yuki takagi <yuki99takagi@gmail.com>
2020-05-06 11:27:29 +0900

    [Add] ログイン、ログアウト機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
