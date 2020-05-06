db7ac245193be7af1658f556225cf42c4a1c054f
yuki takagi <yuki99takagi@gmail.com>
2020-05-06 16:30:12 +0900

    [Add] ユーザー管理機能(一覧、新規ユーザー作成、編集、削除)を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'users/new'
   root to: 'tasks#index'
   resources :tasks
+  namespace :admin do
+    resources :users
+  end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
