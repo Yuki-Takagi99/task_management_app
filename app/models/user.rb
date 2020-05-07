f213720e297727688d21ab65c958f8930405905e
yuki takagi <yuki99takagi@gmail.com>
2020-05-07 13:20:28 +0900

    [Add] emailカラムにユニーク制約を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   before_update :admin_update_check
   has_many :tasks, :dependent => :destroy
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
   before_validation { email.downcase! }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
