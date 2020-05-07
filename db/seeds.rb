f05186034088fecdb112c70650caf3eb0da84631
yuki takagi <yuki99takagi@gmail.com>
2020-05-07 09:03:44 +0900

    [Update] シードデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,25 @@
 User.create!(
   name: 'admin',
   email: 'admin@test.com',
-  password: 'adminadmin',
-  password_confirmation: 'adminadmin',
+  password: '00000000',
+  password_confirmation: '00000000',
   admin: 'true',
 )
 
-5.times do |n|
+3.times do |n|
   User.create!(
-    name: "ユーザー#{n + 1}",
+    name: "user#{n + 1}",
     email: "user#{n + 1}@test.com",
-    password: "useruser#{n + 1}",
-    password_confirmation: "useruser#{n + 1}",
+    password: "00000000",
+    password_confirmation: "00000000",
     admin: "false",
+  )
+end
+
+User.all.each do |user|
+  user.tasks.create!(
+    title: "sample1",
+    description: "sample1",
+    user_id: user.id
   )
 end
