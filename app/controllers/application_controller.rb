65db2ac35d06176cca7ff4b0de8b11ae29c990e3
yuki takagi <yuki99takagi@gmail.com>
2020-05-08 23:09:32 +0900

    [Add] 404,500エラーページを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,36 @@
 class ApplicationController < ActionController::Base
+  # 例外ハンドル
+  unless Rails.env.development?
+    rescue_from Exception,                        with: :render_500
+    rescue_from ActiveRecord::RecordNotFound,     with: :render_404
+    rescue_from ActionController::RoutingError,   with: :render_404
+  end
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+
+    if request.format.to_sym == :json
+      render json: { error: '404 error' }, status: :not_found
+    else
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    end
+  end
+
+  def render_500(e = nil)
+    logger.error "Rendering 500 with exception: #{e.message}" if e
+
+    if request.format.to_sym == :json
+      render json: { error: '500 error' }, status: :internal_server_error
+    else
+      render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+    end
+  end
 end
